ca1e9e18cb89ab0e859aab697ea1a9bc965ff4c8
Will Leinweber <will@bitfission.com>
2013-09-20 13:12:51 +0200

    refresh list each time

--- s3.rb
+++ s3.rb
@@ -1,10 +1,10 @@
 require 'fog'
-conn = Fog::Storage.new(provider: "AWS", aws_access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"), aws_secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"))
-S3 = conn.directories.get( ENV.fetch("BUCKET") )
+CONN = Fog::Storage.new(provider: "AWS", aws_access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"), aws_secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"))
 
 class Video < Struct.new(:name, :url)
   def self.list
-    S3.files.map {|f| new(f.key, f.url( Time.now + 600 ))}
+    s3 = CONN.directories.get( ENV.fetch("BUCKET") )
+    s3.files.map {|f| new(f.key, f.url( Time.now + 600 ))}
   end
 end
 
